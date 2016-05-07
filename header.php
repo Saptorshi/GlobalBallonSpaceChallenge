cc0168836258adb71841d01705f999adbdac1c45
Puranjay Jain <puranjay.jain@st.niituniversity.in>
2016-05-08 00:03:28 +0530

    added balloon animation

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Global Balloon Challenge</title>
+  <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+  <svg class="env-sun">
+    <use xlink:href="svg/sun.svg#svg2"></use>
+  </svg>
+  <svg viewbox="0 0 48 48" class="env-cloud">
+    <use xlink:href="svg/cloud.svg#svg2"></use>
+  </svg>
+  <svg viewbox="0 0 48 48" class="env-cloud-1">
+    <use xlink:href="svg/cloud.svg#svg2"></use>
+  </svg>
+  <svg viewbox="0 0 48 48" class="env-cloud-2">
+    <use xlink:href="svg/cloud.svg#svg2"></use>
+  </svg>
+  <div class="env-balloon">
+    <svg viewbox="0 0 172 256" class="env-balloon-inner">
+      <use xlink:href="svg/balloon.svg#svg2"></use>
+    </svg>
+  </div>
+  <script src="js/jquery-2.2.3.min.js"></script>
+  <script src="js/velocity.js"></script>
+  <script src="js/animations.js"></script>
+  <script src="js/svg4everybody.js"></script>
+</body>
+</html>
